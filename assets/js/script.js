b1eb22f528dcdb24f800440234b272e4f8bd72d2
devasmi <impokhrelasmita5@gmail.com>
2023-01-12 14:19:02 -0500

    added apikey

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
+var showsApikey = "6f740c06220cb598e70409f4b591536e";
+var baseUrl = "https://api.themoviedb.org/3";
+var popularUrl = baseUrl + "/tv/popular?language=en-US&api_key=" + showsApikey;
+
 var charactersButton = document.getElementById("characters-button");
 var detailsButton = document.getElementById("details-button");
 
-charactersButton.onclick = function() {
-    document.location.href= "./characters.html"
-}
+charactersButton.onclick = function () {
+  document.location.href = "./characters.html";
+};
 
-detailsButton.onclick = function() {
-    document.location.href="./details.html"
-}
+detailsButton.onclick = function () {
+  document.location.href = "./details.html";
+};
