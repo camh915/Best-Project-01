7e59792168b12c6bec20b96d955ad601d30f202f
unknown <aguilera.k02@gmail.com>
2023-01-14 17:43:54 -0500

    ladies and gents, we have days, hours, and minutes

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,7 +92,7 @@ function popularShows() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      // console.log(data);
       displayPopularShows(data.results)
     });
 }
@@ -100,7 +100,7 @@ function popularShows() {
 
 
 function displayPopularShows(results) {
-  console.log(results)
+  // console.log(results)
   for (var i = 0; i < 3; i++) {
     var show = results[i];
 
@@ -156,7 +156,7 @@ function storeSavedTitles() {
 function renderTitles() {
   savedList.innerHTML = "";
 
-  console.log(titles);
+  // console.log(titles);
   for (var i = 0; i < titles.length; i++) {
     var a = document.createElement("a"); //<a></a>
     a.setAttribute("class", "list-group-item list-group-item-action");
@@ -171,10 +171,10 @@ function renderTitles() {
 // displays the saved list when page initializes
 function init() {
   var storedTitles = JSON.parse(localStorage.getItem("titles"));
-  console.log(storedTitles)
+  // console.log(storedTitles)
 
   if (storedTitles !== null) {
-    console.log('titles saved')
+    // console.log('titles saved')
     titles = storedTitles;
   }
 
@@ -217,7 +217,7 @@ function addClickHandler() {
 function howManySeasons(tvId) {
   var howManySeasonsUrl = baseUrl + "/tv/" + tvId + "?api_key=6f740c06220cb598e70409f4b591536e&language=en-US";
  
-  fetch(howManySeasonsUrl)
+fetch(howManySeasonsUrl)
 .then(function (response) {
   console.log(response);
   return response.json();
@@ -229,13 +229,13 @@ function howManySeasons(tvId) {
   howManyEpisodes(data.seasons.length);
 }
 );
-}
- 
+
 // for each season of the show, find how many episodes there are
+
 function howManyEpisodes(seasonNumbers) {
  
   for (var i = 1; i < seasonNumbers + 1; i++) {
-    var howManyEpisodesUrl = baseUrl + "/tv/65495/season/" + [i] + "?api_key=6f740c06220cb598e70409f4b591536e&language=en-US";
+    var howManyEpisodesUrl = baseUrl + "/tv/" + tvId +"/season/" + [i] + "?api_key=6f740c06220cb598e70409f4b591536e&language=en-US";
  
   fetch(howManyEpisodesUrl)
   .then(function(response) {
@@ -243,7 +243,9 @@ function howManyEpisodes(seasonNumbers) {
   })
   .then(function(data) {
     runTimes(data.episodes);
+    console.log(data.episodes);
   });
+}
 }
 }
  
@@ -261,6 +263,25 @@ function runTimes(episodes) {
     sum += totalRunTime[i];
   }
  
-  console.log(sum)
+  console.log(sum);
+  timeCalculation(sum);
 }
 
+function timeCalculation(sum) {
+// how many minutes in a day
+  var dayMinutes = (60 * 24)
+
+  // shows us how many days
+  var days = Math.floor(sum/dayMinutes);
+  console.log(days);
+
+  // shows us how many hours
+  var minutesToHours = (sum%dayMinutes);
+ var hours = Math.floor(minutesToHours/60);
+ console.log(hours);
+
+//  shows us how many minutes
+ var remaindingMinutes = (minutesToHours%60);
+ console.log(remaindingMinutes);
+  
+}
